b9751b1512bc197a8bd0f06bc55630443bfdac27
bilguungzt <bilguungzt@gmail.com>
2022-01-23 20:58:24 +0800

    initialise project

---
+++ src/components/WorkData.js
@@ -0,0 +1,24 @@
+export const Work = [
+    {
+        id:1,
+        name:"Web 3.0 Blockchain Application",
+        description:"It is a modern Blockchain application that allow users to access their Ethereum wallet via Meta Mask and interact with decentralized application in their browser without running an Ethereum node.",
+        tags:["React","Smart Contract","Solidity", "ERC-20","Blockchain"],
+        
+        demo:"",
+        github:"https://github.com/bilguungzt"
+    },
+    {
+        id:2,
+        name:"Full Stack Food Ordering Application",
+        description:"Built a responsive online food ordering application using Next.js for server-side rendering and MongoDB to permanently store and track food order. Created a comprehensive admin dashboard with user authentication that allows user to easily interact with all products and pending orders.",
+        tags:["Next.js","MongoDB", "HTML/CSS"],
+        
+        demo:"",
+        github:"https://github.com/bilguungzt"
+        
+    },
+    
+    
+  
+] 
